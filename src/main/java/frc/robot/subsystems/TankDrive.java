377abbf0ae91bc0ec9d37f26790556a880fc7245
Novia <youxiqwtui@gmail.com>
2026-01-28 08:53:58 +0800

    Clean up TankDrive.java by removing comments
    Removed unnecessary comments and a block of code.

--- src/main/java/frc/robot/subsystems/TankDrive.java
+++ src/main/java/frc/robot/subsystems/TankDrive.java
@@ -31,22 +31,3 @@ public class TankDrive extends SubsystemBase {
     }
 
 }
-
-
-
-//SHA256:bxej2kykD8fNkysLbyq+Cs3okcppcHaGfxMKRCUkaXM youxiqwtui@gmail.com
-
-
-/*
- * ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDHZcepEVG9pO+JinQzar1zOr7rK7WZ/N6clXItF17QeWzalw+Bm
- * hMWgIfZWH/jyn4T2fIgyg3NfptsBadweln39r7qwKiqOQWlWTNye3wXmRqt4zhf+wGfuuXMDWGO2mhPhGFFUfEezAyOtomEi
- * wuV6Pu0kc6NMzgOA0K10vclanAo1l/xsjGSn15iQ9tZ+Aad5iMvpprpaM50AfnBcVotmVzfI5aUu4RWLrXPxtBh2vd4cTtwwAMcf
- * rT46Pjg6AMU8Ju1NXqZtYb1Is7Ay2LbqjNFkFmUXQUy6PKGU9yf6ODAsyk8S0d2jVAAhHCawKXprt9VTV/kzZBO6BVhs4JryIN0Fqk
- * ALcAicMriWdzDRefYopmtDx3PwuCtxhE28Bp7IK3dXwUm6a5TS324yxW/JIx3JeIeM+5sn8HbH8L76c0r7Ld9M/o215IBCdRSkpk8qUSG
- * q15D5bGSLwKN1nw2Mro1Tvyl0352tmSdPhsFuIGSDIJUL5fVqA2rVZ38vhwpJ27XCc8C/fxXtcxKWDanVKO0HUlLh7BLo2H
- * TE3qQPx0A8e72xTFzbFEfOWPj+/dAjoK8ZA/peqtmZybj4YbjGiM/uBudiUw3KV7r7QFkrB
- * fMjIy/PwZiiV8X9kLlYFGZA3xwdBV7STUxkczYizOdmpm7Eu58q8gflC9dqBrG+Q== youxiqwtui@gmail.com
- * 
- * 
- * 
- */
